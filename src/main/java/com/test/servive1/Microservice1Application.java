574798a26b193c8759c6510ad0c55e595de33ccb
ashanperera <ashanp@gmail.com>
2024-02-22 18:56:06 +0400

    first commit

---
+++ src/main/java/com/test/servive1/Microservice1Application.java
@@ -0,0 +1,15 @@
+package com.test.servive1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class Microservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservice1Application.class, args);
+	}
+
+}
