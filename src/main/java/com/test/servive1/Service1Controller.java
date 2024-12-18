f5970f40061889b609d8b2d85ca4502b7f57b027
Ashan Malinda Perera <ashanp@gmail.com>
2024-12-18 07:59:58 +0400

    Update Service1Controller.java

--- src/main/java/com/test/servive1/Service1Controller.java
+++ src/main/java/com/test/servive1/Service1Controller.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Service1Controller {
+
+    @Value("${custom.property}")
+    private String customProperty;
 	
     @GetMapping("/hello")
     public String hello() {
+	    System.out.println("Custom Property Value: " + customProperty);
 	    System.out.println("Hello triggered");
         return "Hello from Service 1!";
     }
