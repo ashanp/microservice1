bc41a5ff878e327019672402f7d166b8e50925fc
Ashan Malinda Perera <ashanp@gmail.com>
2024-12-18 08:23:45 +0400

    Update Service1Controller.java

--- src/main/java/com/test/servive1/Service1Controller.java
+++ src/main/java/com/test/servive1/Service1Controller.java
@@ -2,6 +2,7 @@ package com.test.servive1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class Service1Controller {
